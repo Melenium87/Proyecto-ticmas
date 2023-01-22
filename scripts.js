f5f07251118942d8f6f66ee05c187b00eb3a43b0
Melenium87 <0cirosanchez@gmail.com>
2023-01-22 07:27:11 -0300

    je

--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
+//Es lo que hace que la barra te persiga cuadno se la pase
+//Tambien hace que cuando screoleas para abajo desaparezca
+
+let ubicacionPrincipal = window.pageYOffset;
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 713) {
-    var navbar = this.document.querySelector("navbar");
-    navbar.classList.remove("transicion");
+    let desplazamientoActual = window.pageYOffset;
+    if (desplazamientoActual <= ubicacionPrincipal) {
+      var navbar = this.document.querySelector("navbar");
+      navbar.classList.remove("transicion");
+      ubicacionPrincipal = desplazamientoActual;
+    } else {
+      var navbar = this.document.querySelector("navbar");
+      navbar.classList.add("transicion");
+      ubicacionPrincipal = desplazamientoActual;
+    }
   }
 });
+
 window.addEventListener("scroll", function () {
   if (window.pageYOffset < 713) {
     var navbar = this.document.querySelector("navbar");
